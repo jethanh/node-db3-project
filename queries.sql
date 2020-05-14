c1c82c39c732fa3a11d8c5ea2cabc0252e974a15
unknown <jethanhargrave@gmail.com>
2020-05-14 16:35:44 -0400

    mvp

--- queries.sql
+++ queries.sql
@@ -2,8 +2,26 @@
 
 -- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
 
+SELECT p.ProductName, c.CategoryName
+FROM Product AS p
+JOIN Category AS c ON p.CategoryId = c.Id;
+
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
+SELECT o.Id, s.CompanyName
+FROM 'Order' as o
+JOIN 'Shipper' as s ON o.ShipVia = s.Id 
+WHERE o.OrderDate < '2012-08-09'
+
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
+SELECT d.Quantity, p.ProductName
+FROM 'Product' AS p
+JOIN 'OrderDetail' AS d ON d.ProductId = p.Id
+WHERE d.OrderId = 10251
 
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
+
+SELECT o.Id, c.CompanyName, e.LastName
+FROM 'Order' AS o
+JOIN 'Customer' AS c ON o.EmployeeId = e.Id
+JOIN 'Employee' AS e ON o.CustomerId = c.Id 
---
+++ schemes/scheme-model.js
@@ -0,0 +1,61 @@
+const db = require("../data/db.js");
+
+module.exports = {
+  find,
+  findById,
+  findSteps,
+  add,
+  addStep,
+  update,
+  remove
+};
+
+function find() {
+  return db("schemes");
+}
+
+function findById(id) {
+    return db("schemes").where({ id }).first();
+}
+
+function findSteps(id) {
+    return db("steps")
+    .where("scheme_id", id)
+    .join("schemes", "steps.scheme_id", "=", "schemes.id")
+    .select("steps.id", "schemes.scheme_name", "steps.step_number", "steps.instructions")
+    .orderBy("steps.step_number");
+
+} // I should write a findStepById function which finds a specific step for a specific scheme.
+
+// -   `findSteps(id)`:
+//     -   Expects a scheme `id`.
+//     -   Resolves to an array of all correctly ordered step for the given scheme: `[ { id: 17, scheme_name: 'Find the Holy Grail', step_number: 1, instructions: 'quest'}, { id: 18, scheme_name: 'Find the Holy Grail', step_number: 2, instructions: '...and quest'}, etc. ]`.
+//     -   This array should include the `scheme_name` _not_ the `scheme_id`.
+
+function add() {
+    return db("schemes")
+    .insert(scheme, "id")
+    .then(ids => {
+        return findById(ids[0]);
+    });
+}
+
+function update (changes, id) {
+    return db("schemes")
+        .where({ id })
+        .update(changes)
+        .then(() => {
+            return findById(id)
+        })
+}
+
+function addStep(step, id) {
+    return findSteps({ id })
+    .where("steps.scheme_id", id)
+    .insert(step, "scheme_id");
+  }
+
+function remove(id) {
+    return db("schemes")
+        .where({ id }).del();
+}
